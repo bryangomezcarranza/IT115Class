a5abcde4c6ccb50f1c10ab04d8728c843627c9c1
bryangomezcarranza <bryangomezcarranza@gmail.com>
2023-05-25 10:38:46 -0700

    Finished Combobox

--- combobox.py
+++ combobox.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 
 # define function for event 
-def  onSelect(event):
+def onSelect(event):
     # created the event that will be selected and pass it on the print statement. 
     selected_item = event.widget.get()
     print("Selected item:", selected_item)
@@ -11,3 +11,16 @@ def  onSelect(event):
 # Creates a root window and set title 
 rootWindow = tk.Tk()
 rootWindow.title("Button Example")
+
+#
+items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+# 
+combo_box = ttk.Combobox(rootWindow, values=items)
+
+#
+combo_box.bind("<<ComboboxSelected>>", onSelect)
+
+combo_box.pack()
+
+rootWindow.mainloop()
